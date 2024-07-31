ddb6e38a10f900000af359e469fdac9b53cbd333
Maximillian Estrada <maximillian_estrada@yahoo.com>
2024-08-01 01:52:43 +0800

    v0.0.3 - Generate Password
    - Generate random password with given length

--- lumache.py
+++ lumache.py
@@ -1,3 +1,16 @@
+import string
+from random import random
+
+
+def get_random_password(length):
+    """
+    Generates a random password of given length.
+    :param length:
+    :return:
+    """
+    return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(length))
+
+
 def get_random_ingredients(kind=None):
     """
     Return a list of random ingredients as strings.
